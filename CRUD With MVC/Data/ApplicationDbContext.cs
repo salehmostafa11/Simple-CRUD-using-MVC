9888fc67599d17194a7a2b2b1959d47ded34c65b
saleh mostafa 11 <120852885+salehmostafa11@users.noreply.github.com>
2025-10-17 10:23:45 +0300

    Creating the DB

---
+++ CRUD With MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CRUD_With_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_With_MVC.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CRUD With MVC/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_With_MVC.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display(Name ="Student Id")]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [Display(Name ="Student Name")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Course { get; set; }
+        [Display(Name ="Ennrolement Date")]
+        public DateTime EnnrolmentDate { get; set; }
+    }
+}
